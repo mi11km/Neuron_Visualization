4c26d159f8977d3684e59afaf4f66c87349085d8
mi11km <mi11km.basket@gmail.com>
2023-06-09 00:52:42 +0900

    change move speed

--- Assets/Project/Scripts/PlayerMove.cs
+++ Assets/Project/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private float step;
+    private float speed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +16,18 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         // Define step value for animation
-        step = 5.0f * Time.deltaTime;
+        step = 7.0f * Time.deltaTime;
+        speed = 2.0f * Time.deltaTime;
 
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft)) transform.Rotate(0, -5.0f * step, 0);
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight)) transform.Rotate(0, 5.0f * step, 0);
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp)) transform.Rotate( -5.0f * step, 0, 0);
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown)) transform.Rotate(5.0f * step, 0, 0);
 
-        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp)) transform.Translate(Vector3.forward * step);
-        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown)) transform.Translate(-Vector3.forward * step);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp)) transform.Translate(Vector3.forward * speed);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown)) transform.Translate(Vector3.back * speed);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft)) transform.Translate(Vector3.left * speed);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)) transform.Translate(Vector3.right * speed);
+
     }
 }
