eb55ea5ff2549de569a52ca2b471966aebc821be
mi11km <mi11km.basket@gmail.com>
2023-06-24 09:42:33 +0900

    refactor code

--- Assets/Project/Scripts/Neuron.cs
+++ Assets/Project/Scripts/Neuron.cs
@@ -1,77 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
 namespace Project.Scripts
 {
     public class Neuron
     {
-        public Dictionary<int, NeuronCompartment> compartments;
+        public readonly Dictionary<int, NeuronCompartment> Compartments;
 
         public Neuron()
         {
-            compartments = new Dictionary<int, NeuronCompartment>();
-        }
-
-        public void Parse(string swcData)
-        {
-            string[] lines = swcData.Split("\n");
-            foreach (string line in lines)
-            {
-                try
-                {
-                    var neuronCompartment = NeuronCompartment.Parse(line);
-                    compartments.Add(neuronCompartment.id, neuronCompartment);
-                }
-                catch (NeuronParseException e)
-                {
-                    Debug.Log(line);
-                }
-            }
+            Compartments = new Dictionary<int, NeuronCompartment>();
         }
     }
 
     public class NeuronCompartment
     {
-        public int id;
-        public int type;
-        public float positionX;
-        public float positionY;
-        public float positionZ;
-        public float radius;
-        public int parentId;
+        public readonly int ID;
+        public readonly int Type;
+        public readonly float PositionX;
+        public readonly float PositionY;
+        public readonly float PositionZ;
+        public readonly float Radius;
+        public readonly int ParentId;
 
         public NeuronCompartment(int id, int type, float positionX, float positionY, float positionZ, float radius,
             int parentId)
         {
-            this.id = id;
-            this.type = type;
-            this.positionX = positionX;
-            this.positionY = positionY;
-            this.positionZ = positionZ;
-            this.radius = radius;
-            this.parentId = parentId;
-        }
-
-        static public NeuronCompartment Parse(string data)
-        {
-            if (!Regex.IsMatch(data, @"^(\d+\s+){2}(-?\d+\.\d+\s+){4}-?\d+\s*$"))
-            {
-                throw new NeuronParseException("");
-            }
-
-            string[] compartmentDetail = data.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            return new NeuronCompartment(Int32.Parse(compartmentDetail[0]), Int32.Parse(compartmentDetail[1]),
-                float.Parse(compartmentDetail[2]), float.Parse(compartmentDetail[3]), float.Parse(compartmentDetail[4]),
-                float.Parse(compartmentDetail[5]), Int32.Parse(compartmentDetail[6]));
-        }
-    }
-
-    class NeuronParseException : Exception
-    {
-        public NeuronParseException(string message)
-        {
+            ID = id;
+            Type = type;
+            PositionX = positionX;
+            PositionY = positionY;
+            PositionZ = positionZ;
+            Radius = radius;
+            ParentId = parentId;
         }
     }
 }
